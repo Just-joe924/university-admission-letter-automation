91437d4fdceb9073dcdcd517e8c1b4e3e61e6f13
Just-joe924 <theemmanueljohnson@gmail.com>
2026-05-14 12:56:14 +0100

    feat: folder structure and AppRoutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import { Outlet } from "react-router-dom";
 
-function App() {
+export default function App() {
+    return <Outlet/>;
 }
-
-export default App
---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,90 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+
+import App from "../App";
+
+import AdminLayout from "../components/layout/AdminLayout";
+
+import Home from "../pages/Home";
+
+import VerifyAdmission from "../pages/student/VerifyAdmission";
+import AdmissionSuccess from "../pages/student/AdmissionSuccess";
+
+import AdminLogin from "../pages/admin/AdminLogin";
+import Dashboard from "../pages/admin/Dashboard";
+import Students from "../pages/admin/Students";
+import StudentDetails from "../pages/admin/StudentDetails";
+import AdmissionLetters from "../pages/admin/AdmissionLetters";
+
+import NotFound from "../pages/NotFound";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <NotFound />,
+        children: [
+        {
+            index: true,
+            element: <Home />,
+        },
+
+        {
+            path: "student",
+            element: <VerifyAdmission />,
+        },
+
+        {
+            path: "success",
+            element: <AdmissionSuccess />,
+        },
+        ],
+    },
+
+    {
+        path:"/admin/login",
+        element:<AdminLogin/>
+    },
+
+    {
+        path:"/admin/login",
+        element:<AdminSignup/>
+    },
+
+    {
+        path: "/admin",
+        element: <AdminLayout />,
+        children: [
+        {
+            index: true,
+            element: <Dashboard />,
+        },
+
+        {
+            path: "dashboard",
+            element: <Dashboard />,
+        },
+
+        {
+            path: "students",
+            element: <Students />,
+        },
+
+        {
+            path: "students/:id",
+            element: <StudentDetails />,
+        },
+
+        {
+            path: "admission-letters",
+            element: <AdmissionLetters />,
+        },
+        ],
+    },
+
+    {
+        path: "*",
+        element: <NotFound />,
+    },
+]);
+
+export default router;
