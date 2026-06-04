af16af5fc59b2a46dc642db8fde30245f06e2d9b
unknown <theemmanueljohnson@gmail.com>
2026-06-04 09:02:29 +0100

    feat:add dashboard Api

---
+++ frontend/src/services/dashboardApi.js
@@ -0,0 +1,21 @@
+import api from "./api";
+
+export const getDashboardStats = async () => {
+  const response = await api.get("/dashboard/stats");
+  return response.data;
+};
+
+export const getModeOfEntryStats = async () => {
+  const response = await api.get("/dashboard/mode-of-entry");
+  return response.data;
+};
+
+export const getStudentsByDepartment = async () => {
+  const response = await api.get("/dashboard/students-by-departments");
+  return response.data;
+};
+
+export const getRecentStudents = async () => {
+  const response = await api.get("/dashboard/recent-students");
+  return response.data;
+};
